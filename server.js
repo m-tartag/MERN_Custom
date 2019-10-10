ded23cad113a9599f17044006c90b2e196b46768
m-tartag <mattbrs51@gmail.com>
2019-10-10 12:10:24 -0400

    fleshing out routes

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const chalk = require('chalk');
+const connectDB = require('./config/db');
 
+// Init App
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(chalk.green.inverse(`Server started on port: ${PORT}`));
+  console.log(chalk.blue.inverse(`Server Connected @ Port: ${PORT}`));
 });
